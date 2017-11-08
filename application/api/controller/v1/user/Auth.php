60bbea3a25ffdf34e71568e2e95179085799494d
李隽文 <617450941@qq.com>
2017-11-08 09:14:07 +0800

    增加包

--- application/api/controller/v1/user/Auth.php
+++ application/api/controller/v1/user/Auth.php
@@ -1,7 +1,18 @@
 <?php
 namespace app\api\controller\v1\User;
+use \think\Request;
 
-//授权开户相关操作
+//授权登录相关操作
 class Auth {
-
+    /**
+     * 账号登录
+     * 
+     * 
+     */
+    public function login () {
+        $data = input('put.');
+        
+        return \think\Loader::model('AuthModel','logic\v1\user')->login($data);
+    }
+    
 }
--- application/api/logic/v1/user/AuthModel.php
+++ application/api/logic/v1/user/AuthModel.php
@@ -4,5 +4,8 @@ use think\Model;
 use think\Db;
 
 class AuthModel extends Model {
-    
+    public function login ($data) {
+
+        return msg(200,'success',['aa'=>123]);
+    }
 }
