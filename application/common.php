3b22b531483477c649956e1735df909601a78ca7
李隽文 <617450941@qq.com>
2017-11-14 15:01:44 +0800

    修改返回值

--- application/common.php
+++ application/common.php
@@ -2,7 +2,7 @@
 //api返回数据方法
 function msg($code,$msg,$arr = ''){
     $result['meta']['code'] = $code;
-    $result['meta']['msg'] = $msg;
+    $result['meta']['message'] = $msg;
     $result['body'] = $arr != '' ? $arr : null;
     return $result;
 }
