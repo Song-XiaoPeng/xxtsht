ccb46d5b661baf0c9531c38dd4d7a1479d144d3e
李隽文 <617450941@qq.com>
2017-12-04 16:47:50 +0800

    增加获取客户提醒列表api

--- application/api/controller/v1/message/Remind.php
+++ application/api/controller/v1/message/Remind.php
@@ -7,7 +7,7 @@ class Remind extends Auth{
 	/**
      * 增加客户提醒
      * 请求类型 post
-	 * 传入JSON格式: {"remind_content":"测试","wx_user_id":"0b45c030270e41d0d873713077107ad1","remind_time":"2017-12-08 20:20:12"}
+	 * 传入JSON格式: {"remind_content":"测试","wx_user_id":"0b45c030270e41d0d873713077107ad1","remind_time":"2017-12-08 20:20:12","remind_openid":"oF_-jjmYyxKMsnmN-z0mRWgsLeQI"}
 	 * 返回JSON格式: {"meta":{"code":200,"message":"success"},"body":null}
 	 * API_URL_本地: http://localhost:91/api/v1/message/Remind/addRemind
 	 * API_URL_服务器: http://kf.lyfz.net/api/v1/message/Remind/addRemind
@@ -22,5 +22,24 @@ class Remind extends Auth{
 		$data['uid'] = $this->uid;
 
 		return \think\Loader::model('RemindLogic','logic\v1\message')->addRemind($data);
+	}
+	
+	/**
+     * 获取客户提醒列表
+     * 请求类型 post
+	 * 传入JSON格式: {"wx_user_id": 12,"page":1}
+	 * 返回JSON格式: {"meta":{"code":200,"message":"success"},"body":{"data_list":[{"remind_id":1,"remind_content":"测试","company_id":"51454009d703c86c91353f61011ecf2f","uid":6454,"add_time":"2017-12-04 15:23:38","remind_time":"2017-12-08 20:20:12","wx_user_id":"0b45c030270e41d0d873713077107ad1","remind_openid":"oF_-jjmYyxKMsnmN-z0mRWgsLeQI","wx_user_info":null,"customer_info":null},{"remind_id":2,"remind_content":"测试","company_id":"51454009d703c86c91353f61011ecf2f","uid":6454,"add_time":"2017-12-04 15:33:54","remind_time":"2017-12-08 20:20:12","wx_user_id":"0b45c030270e41d0d873713077107ad1","remind_openid":"oF_-jjmYyxKMsnmN-z0mRWgsLeQI","wx_user_info":null,"customer_info":null},{"remind_id":3,"remind_content":"测试","company_id":"51454009d703c86c91353f61011ecf2f","uid":6454,"add_time":"2017-12-04 15:34:42","remind_time":"2017-12-08 20:20:12","wx_user_id":"0b45c030270e41d0d873713077107ad1","remind_openid":"oF_-jjmYyxKMsnmN-z0mRWgsLeQI","wx_user_info":null,"customer_info":null}],"page_data":{"count":3,"rows_num":16,"page":1}}}
+	 * API_URL_本地: http://localhost:91/api/v1/message/Remind/getRemindList
+	 * API_URL_服务器: http://kf.lyfz.net/api/v1/message/Remind/getRemindList
+	 * @param wx_user_id 提醒的客户微信基础信息id(选传)
+	 * @param page 分页参数默认1
+	 * @return code 200->成功
+	 */
+	public function getRemindList(){
+		$data = input('put.');
+		$data['company_id'] = $this->company_id;
+		$data['uid'] = $this->uid;
+
+		return \think\Loader::model('RemindLogic','logic\v1\message')->getRemindList($data);
     }
 }
--- application/api/logic/v1/message/RemindLogic.php
+++ application/api/logic/v1/message/RemindLogic.php
@@ -19,6 +19,7 @@ class RemindLogic extends Model {
         $remind_time = $data['remind_time'];
         $uid = $data['uid'];
         $company_id = $data['company_id'];
+        $remind_openid = $data['remind_openid'];
 
         $wx_user_res = Db::name('wx_user')
         ->partition('', '', ['type'=>'md5','num'=>config('separate')['wx_user']])
@@ -36,6 +37,7 @@ class RemindLogic extends Model {
             'company_id' => $company_id,
             'add_time' => date('Y-m-d H:i:s'),
             'remind_time' => $remind_time,
+            'remind_openid' => $remind_openid,
         ]);
 
         if($insert_res){
@@ -43,5 +45,86 @@ class RemindLogic extends Model {
         }else{
             return msg(3001,'插入数据失败');
         }
+    }
+
+    /**
+     * 获取客户提醒列表
+     * @param page 分页参数默认1
+     * @param wx_user_id 提醒的客户微信基础信息id
+	 * @param uid 账号uid
+	 * @param company_id 商户company_id
+	 * @return code 200->成功
+	 */
+    public function getRemindList($data){
+        $company_id =$data['company_id'];
+        $wx_user_id =$data['wx_user_id'];
+        $uid =$data['uid'];
+        $page =$data['page'];
+
+        //分页
+        $page_count = 16;
+        $show_page = ($page - 1) * $page_count;
+
+        if($wx_user_id){
+            $map['wx_user_id'] = $wx_user_id;
+        }
+        $map['company_id'] = $company_id;
+        $map['uid'] = $uid;
+
+        $list = Db::name('remind')->where($map)->limit($show_page,$page_count)->select();
+        $count = Db::name('remind')->where($map)->count();
+
+        foreach($list as $k=>$v){
+            $wx_user_info = Db::name('wx_user')
+            ->partition('', '', ['type'=>'md5','num'=>config('separate')['wx_user']])
+            ->where(['wx_user_id'=>$v['wx_user_id']])
+            ->field('nickname,portrait,qrcode_id,customer_info_id,groupid,subscribe_time,appid,subscribe')
+            ->cache(true,10)
+            ->find();
+            if(!$wx_user_info){
+                $list[$k]['wx_user_info'] = null;
+                $list[$k]['customer_info'] = null;
+                continue;
+            }
+
+            $customer_info_res = Db::name('customer_info')
+            ->partition('', '', ['type'=>'md5','num'=>config('separate')['customer_info']])
+            ->where(['customer_info_id'=>$wx_user_info['customer_info_id']])
+            ->cache(true,10)
+            ->find();
+            if(!$customer_info_res){
+                $list[$k]['wx_user_info'] = null;
+                $list[$k]['customer_info'] = null;
+                continue;
+            }
+
+            if($customer_info_res['wx_company_id']){
+                $customer_info_res['wx_comapny_name'] = Db::name('wx_user_company')->where(['wx_company_id'=>$customer_info_res['wx_company_id']])->cache(true,10)->value('wx_comapny_name');
+            }else{
+                $customer_info_res['wx_comapny_name'] = null;
+            }
+
+            if($customer_info_res['wx_user_group_id']){
+                $customer_info_res['wx_user_group_name'] = Db::name('wx_user_group')->where(['wx_user_group_id'=>$customer_info_res['wx_user_group_id']])->cache(true,10)->value('group_name');
+            }else{
+                $customer_info_res['wx_user_group_name'] = null;
+            }
+
+            if($customer_info_res['wx_user_group_id']){
+                $customer_info_res['product_name'] = Dn::name('product')->where(['product_id'=>$customer_info_res['product_id']])->cache(true,10)->value('product_name');
+            }else{
+                $customer_info_res['product_name'] = null;
+            }
+
+            $list[$k]['customer_info'] = $customer_info_res;
+            $list[$k]['wx_user_info'] = $wx_user_info_res;
+        }
+
+        $res['data_list'] = count($list) == 0 ? array() : $list;
+        $res['page_data']['count'] = $count;
+        $res['page_data']['rows_num'] = $page_count;
+        $res['page_data']['page'] = $page;
+        
+        return msg(200,'success',$res);
     }
 }
