ed716e0093ec5f12b33ffccf422ee5b8fad3a2aa
李隽文 <617450941@qq.com>
2017-12-15 13:10:37 +0800

    增加规则设置

--- application/api/controller/v1/setting/Rule.php
+++ application/api/controller/v1/setting/Rule.php
@@ -7,13 +7,13 @@ class Rule extends Auth{
 	/**
      * 设置客资领取规则
      * 请求类型 post
-	 * 传入JSON格式: {"page":"1"}
-	 * 返回JSON格式: {"meta":{"code":200,"message":"success"},"body":""}
+	 * 传入JSON格式: {"cued_pool":{"cycle":"1","number":"1"},"cued_pool_recovery":3,"intention_receive":{"cycle":"1","number":"1"},"intention_recovery":3}
+	 * 返回JSON格式: {"meta":{"code":200,"message":"success"},"body":null}
 	 * API_URL_本地: http://localhost:91/api/v1/setting/Rule/setCustomerResourcesRule
 	 * API_URL_服务器: http://kf.lyfz.net/api/v1/setting/Rule/setCustomerResourcesRule
-     * @param cued_pool 线索池领取周期 {"cycle":"1","number":"1"}
+     * @param cued_pool 线索池领取周期 {"cycle":"1","number":"1"} cycle->1本天 2本周 3本月 number->限制领取数量
      * @param cued_pool_recovery 线索池回收周期 单位天
-     * @param intention_receive 意向领取周期 {"cycle":"1","number":"1"}
+     * @param intention_receive 意向领取周期 {"cycle":"1","number":"1"} cycle->1本天 2本周 3本月 number->限制领取数量
      * @param intention_recovery 意向回收周期 单位天
 	 * @return code 200->成功
 	 */
