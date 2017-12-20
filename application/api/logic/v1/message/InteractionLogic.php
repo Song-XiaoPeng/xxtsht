142cce53df628fbda37f8bff058a5b0671cba823
李隽文 <617450941@qq.com>
2017-12-20 17:24:47 +0800

    修改分页每页数量

--- application/api/logic/v1/message/InteractionLogic.php
+++ application/api/logic/v1/message/InteractionLogic.php
@@ -198,7 +198,7 @@ class InteractionLogic extends Model {
         $session_id = $data['session_id'];
 
         //分页
-        $page_count = 200;
+        $page_count = 40;
         $show_page = ($page - 1) * $page_count;
 
         $list = Db::name('message_data')
