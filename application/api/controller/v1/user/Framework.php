80bb857fdc5d24a2a8078fda9d48c57eae7e25ac
李隽文 <617450941@qq.com>
2017-12-26 14:22:24 +0800

    获取岗位列表api

---
+++ application/api/controller/v1/user/Framework.php
@@ -0,0 +1,89 @@
+<?php
+namespace app\api\controller\v1\user;
+use app\api\common\Auth;
+
+class Framework extends Auth{
+    /**
+     * 添加部门
+     * 请求类型 post
+	 * 传入JSON格式: {"user_group_id":"user_group_id...","department_name":"department_name...","parent_id":"parent_id...","desc":"desc.."}
+	 * 返回JSON格式: {"meta":{"code":200,"message":"success"}}
+	 * API_URL_本地: http://localhost:91/api/v1/user/Framework/addDepartment
+	 * API_URL_服务器: http://kf.lyfz.net/api/v1/user/Framework/addDepartment
+	 * @param user_group_id 选传存在则修改
+	 * @param department_name 部门名称
+	 * @param parent_id 上级部门id
+	 * @param desc 部门描述
+	 * @return code 200->成功
+	 */
+    public function addDepartment () {
+        $data = input('put.');
+        $data['company_id'] = $this->company_id;
+        
+        return \think\Loader::model('FrameworkLogic','logic\v1\user')->addDepartment($data);
+	}
+	
+    /**
+     * 获取部门列表
+     * 请求类型 get
+	 * 返回JSON格式: {"meta":{"code":200,"message":"success"}}
+	 * API_URL_本地: http://localhost:91/api/v1/user/Framework/getDepartmentList
+	 * API_URL_服务器: http://kf.lyfz.net/api/v1/user/Framework/getDepartmentList
+	 * @param company_id 商户id
+	 * @return code 200->成功
+	 */
+    public function getDepartmentList () {       
+        return \think\Loader::model('FrameworkLogic','logic\v1\user')->getDepartmentList($this->company_id);
+	}
+	
+    /**
+     * 添加岗位
+     * 请求类型 post
+	 * 传入JSON格式: {"position_name":"测试","user_group_id":"372"}
+	 * 返回JSON格式: {"meta":{"code":200,"message":"success"},"body":{"position_id":"4"}}
+	 * API_URL_本地: http://localhost:91/api/v1/user/Framework/addPosition
+	 * API_URL_服务器: http://kf.lyfz.net/api/v1/user/Framework/addPosition
+	 * @param position_name 岗位名称
+	 * @return code 200->成功
+	 */
+    public function addPosition () {
+        $data = input('put.');
+        $data['company_id'] = $this->company_id;
+        
+        return \think\Loader::model('FrameworkLogic','logic\v1\user')->addPosition($data);
+	}
+
+    /**
+     * 删除岗位
+	 * 请求类型 post
+	 * 传入JSON格式: {"position_id":"12"}
+	 * 返回JSON格式: {"meta":{"code":200,"message":"success"},"body":""}
+	 * API_URL_本地: http://localhost:91/api/v1/user/Framework/delPosition
+	 * API_URL_服务器: http://kf.lyfz.net/api/v1/user/Framework/delPosition
+	 * @param position_id 岗位id
+	 * @return code 200->成功
+	 */
+	public function delPosition(){
+        $data = input('put.');
+        $data['company_id'] = $this->company_id;
+        
+        return \think\Loader::model('FrameworkLogic','logic\v1\user')->delPosition($data);
+	}
+
+    /**
+     * 获取岗位列表
+	 * 请求类型 post
+	 * 传入JSON格式: {"user_group_id":"372"}
+	 * 返回JSON格式: {"meta":{"code":200,"message":"success"},"body":[{"position_id":5,"position_name":"测试","user_group_id":372,"company_id":"51454009d703c86c91353f61011ecf2f"},{"position_id":6,"position_name":"程序员","user_group_id":372,"company_id":"51454009d703c86c91353f61011ecf2f"}]}
+	 * API_URL_本地: http://localhost:91/api/v1/user/Framework/getPositionList
+	 * API_URL_服务器: http://kf.lyfz.net/api/v1/user/Framework/getPositionList
+	 * @param user_group_id 部门id
+	 * @return code 200->成功
+	 */
+	public function getPositionList(){
+        $data = input('put.');
+        $data['company_id'] = $this->company_id;
+        
+        return \think\Loader::model('FrameworkLogic','logic\v1\user')->getPositionList($data);
+	}
+}
---
+++ application/api/logic/v1/user/FrameworkLogic.php
@@ -0,0 +1,117 @@
+<?php
+namespace app\api\logic\v1\user;
+use think\Model;
+use think\Db;
+use app\api\common\Common;
+
+class FrameworkLogic extends Model {
+    /**
+     * 添加部门
+	 * @param user_group_id 选传存在则修改
+	 * @param department_name 部门名称
+	 * @param parent_id 上级部门id
+	 * @param desc 部门描述
+	 * @param company_id 商户id
+	 * @return code 200->成功
+	 */
+    public function addDepartment($data){
+        $user_group_id = empty($data['user_group_id']) == true ? '' : $data['user_group_id'];
+        $department_name = $data['department_name'];
+        $parent_id = empty($data['parent_id']) == true ? '' : $data['parent_id'];
+        $desc = $data['desc'];
+        $company_id = $data['company_id'];
+
+        if($user_group_id){
+            Db::name('user_group')
+            ->where(['company_id'=>$company_id,'user_group_id'=>$user_group_id])
+            ->update([
+                'user_group_name' => $department_name,
+                'parent_id' => $parent_id,
+                'desc' => $desc
+            ]);
+        }else{
+            $user_group_id = Db::name('user_group')->insertGetId([
+                'user_group_name' => $department_name,
+                'company_id' => $company_id,
+                'parent_id' => $parent_id,
+                'desc' => $desc
+            ]);
+        }
+
+        if($user_group_id){
+            return msg(200,'success',['user_group_id'=>$user_group_id]);
+        }else{
+            return msg(3001,'插入数据失败');
+        }
+    }
+
+    /**
+     * 获取部门列表
+	 * @param company_id 商户id
+	 * @return code 200->成功
+	 */
+    public function getDepartmentList($company_id){
+        $list = Db::name('user_group')->where(['company_id'=>$company_id])->select();
+
+        return msg(200,'success',$list);
+    }
+
+    /**
+     * 添加岗位
+	 * @param company_id 商户id
+	 * @param position_name 岗位名称
+	 * @param user_group_id 所属部门id
+	 * @return code 200->成功
+	 */
+    public function addPosition($data){
+        $company_id = $data['company_id'];
+        $position_name = $data['position_name'];
+        $user_group_id = $data['user_group_id'];
+
+        $position_id = Db::name('position')->insertGetId([
+            'company_id' => $company_id,
+            'position_name' => $position_name,
+            'user_group_id' => $user_group_id
+        ]);
+
+        if($position_id){
+            return msg(200, 'success', ['position_id'=>$position_id]);
+        }else{
+            return msg(3001,'插入数据失败');
+        }
+    }
+
+    /**
+     * 删除岗位
+	 * @param company_id 商户id
+	 * @param position_id 岗位id
+	 * @return code 200->成功
+	 */
+    public function delPosition($data){
+        $company_id = $data['company_id'];
+        $position_id = $data['position_id'];
+
+        $del_res = Db::name('position')->where(['position_id'=>$position_id,'company_id'=>$company_id])->delete();
+
+        if($del_res){
+            return msg(200, 'success');
+        }else{
+            return msg(3001,'删除数据失败');
+        }
+    }
+
+    /**
+     * 获取岗位列表
+	 * @param company_id 商户id
+	 * @param user_group_id 部门id
+	 * @return code 200->成功
+	 */
+    public function getPositionList($data){
+        $company_id = $data['company_id'];
+        $user_group_id = $data['user_group_id'];
+
+        $list = Db::name('position')->where(['company_id'=>$company_id,'user_group_id'=>$user_group_id])->select();
+
+        return msg(200, 'success', $list);
+    }
+}
