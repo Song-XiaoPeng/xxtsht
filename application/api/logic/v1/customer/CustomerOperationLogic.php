7abeabf6a6e0889495298933e537a28d5c00efe7
李隽文 <617450941@qq.com>
2017-12-30 19:58:27 +0800

    增加获取订单客户数据统计api

--- application/api/logic/v1/customer/CustomerOperationLogic.php
+++ application/api/logic/v1/customer/CustomerOperationLogic.php
@@ -716,11 +716,7 @@ class CustomerOperationLogic extends Model {
         ->order('subscribe_time desc')
         ->buildSql();
 
-        if ($type == 1) {
-            $customer_info_map['is_clue'] = 2;
-        }else{
-            $customer_info_map['is_clue'] = 3;
-        }
+        $customer_info_map['is_clue'] = 4;
 
         $customer_info_map['real_name'] = array('like',"%$real_name%");
 
@@ -784,6 +780,50 @@ class CustomerOperationLogic extends Model {
         $res['page_data']['page'] = $page;
         
         return msg(200,'success',$res);
+    }
+
+    /**
+     * 获取订单客户数据统计
+     * @param company_id 商户company_id
+     * @param uid 登录账号uid
+	 * @return code 200->成功
+	 */
+    public function getOrderCustomerData($company_id, $uid, $user_type){
+        if($user_type != 3){
+            //获取我的团队账号
+            $uid_res = Common::getAscriptionUidList($company_id, $uid, $user_type);
+            if($uid_res['meta']['code'] != 200){
+                return $uid_res;
+            }
+
+            $uid_list = $uid_res['body'];
+            array_push($uid_list, $uid);
+
+            $map['customer_service_uid'] = ['in', $uid_list];
+        }
+
+        //获取订单客户数据
+        $map['company_id'] = $company_id;
+        $map['is_clue'] = 4;
+        $total = Db::name('wx_user')
+        ->partition([], "", ['type'=>'md5','num'=>config('separate')['wx_user']])
+        ->where($map)
+        ->count();
+
+        //获取今日订单客户数据
+        $yesterday_res = getDayTimeSolt();
+        $begin_time = $yesterday_res['begin_time'];
+        $end_time = $yesterday_res['end_time'];
+        $today = Db::query("SELECT COUNT(*) AS count FROM ( SELECT * FROM tb_wx_user_1 UNION SELECT * FROM tb_wx_user_2 UNION SELECT * FROM tb_wx_user_3 UNION SELECT * FROM tb_wx_user_4 UNION SELECT * FROM tb_wx_user_5) AS wx_user WHERE  `company_id` = '$company_id'  AND `is_clue` = 4  AND `set_clue_time` BETWEEN '$begin_time' AND '$end_time' LIMIT 1")[0]['count'];
+
+        $arr = [
+            'total' => $total,
+            'today' => $today,
+            'follow_up' => 0,
+            'intention' => 0
+        ];
+        
+        return msg(200,'success',$arr);
     }
 
     /**
