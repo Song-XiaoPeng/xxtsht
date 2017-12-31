65022da10b15ea5a60d1ddd11f8b660ba327ab70
李隽文 <617450941@qq.com>
2017-12-31 14:54:50 +0800

    增加客户信息

--- application/api/controller/v1/customer/CustomerOperation.php
+++ application/api/controller/v1/customer/CustomerOperation.php
@@ -131,8 +131,11 @@ class CustomerOperation extends Auth{
 	 */
 	public function receiveIntention(){
         $data = input('put.');
-        $data['company_id'] = $this->company_id;
-        $data['uid'] = $this->uid;
+		$data['company_id'] = $this->company_id;
+
+        if(empty($data['uid'])){
+            $data['uid'] = $this->uid;
+        }
         
         return \think\Loader::model('CustomerOperationLogic','logic\v1\customer')->receiveIntention($data);
 	}
