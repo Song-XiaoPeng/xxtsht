7c34218f7bb07c18b623ce18541e1d906337c0cd
李隽文 <617450941@qq.com>
2018-01-05 16:44:25 +0800

    增加数据缓存

--- application/api/logic/v1/message/CommonLogic.php
+++ application/api/logic/v1/message/CommonLogic.php
@@ -231,7 +231,7 @@ class CommonLogic extends Model {
             foreach($tagid_list as $v){
                 $label_id = Db::name('label_tag')->where(['tag_id'=>$v])->cache(true,60)->value('label_id');
                 if($label_id){
-                    $label_res = Db::name('label')->where(['label_id'=>$label_id])->find();
+                    $label_res = Db::name('label')->where(['label_id'=>$label_id])->cache(true,60)->find();
     
                     array_push($label_arr,$label_res);
                 }
