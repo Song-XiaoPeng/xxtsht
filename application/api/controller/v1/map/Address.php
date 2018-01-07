8b172c66d553466da36eb9cf9a3df98ca61e5230
李隽文 <617450941@qq.com>
2018-01-07 15:28:04 +0800

    增加腾讯地图逆地址解析api

--- application/api/controller/v1/map/Address.php
+++ application/api/controller/v1/map/Address.php
@@ -3,5 +3,20 @@ namespace app\api\controller\v1\map;
 use app\api\common\Auth;
 
 class Address extends Auth{
-    
+    /**
+     * 腾讯地图逆地址解析
+     * 请求类型 post
+	 * 传入JSON格式: {"lat":"lat..","lng":"lng.."}
+	 * 返回JSON格式: {"meta":{"code":200,"message":"success"},"body":null}
+	 * API_URL_本地: http://localhost:91/api/v1/map/Address/geocoder
+	 * API_URL_服务器: http://kf.lyfz.net/api/v1/map/Address/geocoder
+	 * @param lat 纬度
+	 * @param lng 经度
+	 * @return code 200->成功
+	 */
+    public function geocoder(){
+        $data = input('put.');
+        
+        return \think\Loader::model('AddressLogic','logic\v1\map')->geocoder($data['lat'],$data['lng']);
+    }
 }
---
+++ application/api/logic/v1/map/AddressLogic.php
@@ -0,0 +1,24 @@
+<?php
+namespace app\api\logic\v1\map;
+use think\Model;
+use think\Db;
+use app\api\common\Common;
+
+class AddressLogic extends Model {
+    /**
+     * 腾讯地图逆地址解析
+	 * @param lat 纬度
+	 * @param lng 经度
+	 * @return code 200->成功
+	 */
+    public function geocoder($lat,$lng){
+        $url = 'http://apis.map.qq.com/ws/geocoder/v1/?location='.$lat.','.$lng.'&key='.config('web_service_api');
+
+        $client = new \GuzzleHttp\Client();
+        $res = $client->request('GET', $url);
+
+        $arr = json_decode($res->getBody(),true);
+
+        return msg(200,'success',$arr['result']);
+    }
+}
