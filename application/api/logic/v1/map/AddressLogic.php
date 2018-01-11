0f1577eb0b3122e9ac2864841429701f153e46f6
李隽文 <617450941@qq.com>
2018-01-11 16:28:13 +0800

    增加数据过滤

--- application/api/logic/v1/map/AddressLogic.php
+++ application/api/logic/v1/map/AddressLogic.php
@@ -12,6 +12,10 @@ class AddressLogic extends Model {
 	 * @return code 200->成功
 	 */
     public function geocoder($lat,$lng){
+        if(empty($lat) == true || empty($lng) == true){
+            return msg(3001, '暂无相关地址信息');
+        }
+
         $url = 'http://apis.map.qq.com/ws/geocoder/v1/?location='.$lat.','.$lng.'&key='.config('web_service_api');
 
         $client = new \GuzzleHttp\Client();
