b07f3edd5f780fab703e923f09eee8a00fa9258d
李隽文 <617450941@qq.com>
2018-01-21 15:04:17 +0800

    增加设置线索客户时间

--- application/api/logic/v1/message/CommonLogic.php
+++ application/api/logic/v1/message/CommonLogic.php
@@ -141,7 +141,7 @@ class CommonLogic extends Model {
                 Db::name('wx_user')
                 ->partition(['company_id'=>$company_id], "company_id", ['type'=>'md5','num'=>config('separate')['wx_user']])
                 ->where(['appid'=>$val['appid'],'openid'=>$val['customer_wx_openid']])
-                ->update(['customer_service_uid'=>$uid,'is_clue'=>-1,'last_time'=>$time]);
+                ->update(['customer_service_uid'=>$uid,'is_clue'=>-1,'last_time'=>$time,'set_clue_time'=>$time]);
 
                 if($update_res){
                     $redis->SREM($company_id, $v);
