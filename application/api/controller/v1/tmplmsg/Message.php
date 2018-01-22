2c7a3595482e62c1665adc0bc97a9f15860d4815
李隽文 <617450941@qq.com>
2018-01-22 11:15:34 +0800

    增加删除消息模板api

--- application/api/controller/v1/tmplmsg/Message.php
+++ application/api/controller/v1/tmplmsg/Message.php
@@ -4,9 +4,9 @@ use app\api\common\Auth;
 
 class Message extends Auth{
 	/**
-     * 获取所有模板列表
+     * 获取所有消息模板列表
      * 请求类型 post
-	 * 请求JSON格式: {"appid":"wxe30d2c612847beeb"}
+	 * 请求JSON格式: {"appid":"wx4a14a2375e93cb7b"}
 	 * 返回JSON格式: 
 	 * API_URL_本地: http://localhost:91/api/v1/tmplmsg/Message/getAllTemplateList
 	 * API_URL_服务器: http://kf.lyfz.net/api/v1/tmplmsg/Message/getAllTemplateList
@@ -18,5 +18,41 @@ class Message extends Auth{
 		$data['company_id'] = $this->company_id;
 
 		return \think\Loader::model('MessageLogic','logic\v1\tmplmsg')->getAllTemplateList($data);
+	}
+
+	/**
+     * 添加消息模板
+     * 请求类型 post
+	 * 请求JSON格式: {"appid":"wx4a14a2375e93cb7b","short_id":"TM00015"}
+	 * 返回JSON格式: 
+	 * API_URL_本地: http://localhost:91/api/v1/tmplmsg/Message/addTemplate
+	 * API_URL_服务器: http://kf.lyfz.net/api/v1/tmplmsg/Message/addTemplate
+     * @param appid 公众号appid
+     * @param short_id 添加的模板ID
+	 * @return code 200->成功
+	 */
+	public function addTemplate(){
+		$data = input('put.');
+		$data['company_id'] = $this->company_id;
+
+		return \think\Loader::model('MessageLogic','logic\v1\tmplmsg')->addTemplate($data);
+	}
+
+	/**
+     * 删除消息模板
+     * 请求类型 post
+	 * 请求JSON格式: {"appid":"wx4a14a2375e93cb7b","template_id":"BQrJpIHncCAk3BBDuLSAL_2VhWLd1eWLiKkkHiLWRYc"}
+	 * 返回JSON格式: 
+	 * API_URL_本地: http://localhost:91/api/v1/tmplmsg/Message/delTemplate
+	 * API_URL_服务器: http://kf.lyfz.net/api/v1/tmplmsg/Message/delTemplate
+     * @param appid 公众号appid
+     * @param template_id 删除的模板ID
+	 * @return code 200->成功
+	 */
+	public function delTemplate(){
+		$data = input('put.');
+		$data['company_id'] = $this->company_id;
+
+		return \think\Loader::model('MessageLogic','logic\v1\tmplmsg')->delTemplate($data);
 	}
 }
--- application/api/logic/v1/tmplmsg/MessageLogic.php
+++ application/api/logic/v1/tmplmsg/MessageLogic.php
@@ -7,7 +7,7 @@ use app\api\common\Common;
 
 class MessageLogic extends Model {
     /**
-     * 获取所有模板列表
+     * 获取所有消息模板列表
      * @param company_id 所属商户company_id
      * @param appid 公众号appid
 	 * @return code 200->成功
@@ -40,6 +40,73 @@ class MessageLogic extends Model {
             $template_list = [];
         }
 
-        return msg(200,'success',$template_list);
+        return msg(200,'success',array_reverse($template_list));
     }
+
+    /**
+     * 添加消息模板
+     * @param company_id 所属商户company_id
+     * @param appid 公众号appid
+     * @param short_id 添加的模板ID
+	 * @return code 200->成功
+	 */
+    public function addTemplate($data){
+        $company_id = $data['company_id'];
+        $appid = $data['appid'];
+        $short_id = $data['short_id'];
+
+        try {
+            $token_info = Common::getRefreshToken($appid,$company_id);
+            if($token_info['meta']['code'] == 200){
+                $refresh_token = $token_info['body']['refresh_token'];
+            }else{
+                return $token_info;
+            }
+
+            $app = new Application(wxOptions());
+            $openPlatform = $app->open_platform;
+
+            $notice = $openPlatform->createAuthorizerApplication($appid,$refresh_token)->notice;
+
+            $add_res = $notice->addTemplate($short_id);
+        } catch (\Exception $e) {
+            return msg(3010,$e->getMessage());
+        }
+
+        return msg(200,'success',['template_id'=>$add_res['template_id']]);
+    }
+
+    /**
+     * 删除消息模板
+     * @param company_id 所属商户company_id
+     * @param appid 公众号appid
+     * @param template_id 删除的模板ID
+	 * @return code 200->成功
+	 */
+    public function delTemplate($data){
+        $company_id = $data['company_id'];
+        $appid = $data['appid'];
+        $template_id = $data['template_id'];
+
+        try {
+            $token_info = Common::getRefreshToken($appid,$company_id);
+            if($token_info['meta']['code'] == 200){
+                $refresh_token = $token_info['body']['refresh_token'];
+            }else{
+                return $token_info;
+            }
+
+            $app = new Application(wxOptions());
+            $openPlatform = $app->open_platform;
+
+            $notice = $openPlatform->createAuthorizerApplication($appid,$refresh_token)->notice;
+            
+            $del_res = $notice->deletePrivateTemplate($template_id);
+        } catch (\Exception $e) {
+            return msg(3010,$e->getMessage());
+        }
+
+        return msg(200,'success');
+    }
+
 }
