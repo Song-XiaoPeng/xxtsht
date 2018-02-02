dec2b81ea563266d6cc926de3017066938955c35
李隽文 <617450941@qq.com>
2018-02-02 17:37:58 +0800

    修复判断错误

--- application/api/logic/v1/customer/CustomerOperationLogic.php
+++ application/api/logic/v1/customer/CustomerOperationLogic.php
@@ -146,7 +146,7 @@ class CustomerOperationLogic extends Model {
                 'add_time' => date('Y-m-d H:i:s'),
             ]);
         }else{
-            if($wx_user_res['customer_service_uid'] !== $uid){
+            if($wx_user_res['customer_service_uid'] != $uid){
                 return msg(3016, '非此客户的专属客服不允许修改信息');
             }
 
