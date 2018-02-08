8912720a5e63cb2ff07af29940779ae5b4b51361
李隽文 <617450941@qq.com>
2018-02-08 16:35:27 +0800

    去除错误的编码

--- extend/Coroutine.php
+++ extend/Coroutine.php
@@ -36,88 +36,80 @@ class Coroutine {
     }
 
     private function stopTick(){
-        if($this->tickId >= 0) {
-            swoole_timer_clear($this->tickId);
-        }
-    }
+        if($this->tickId >= 0) {
+            swoole_timer_clear($this->tickId);
+        }
+    }
 
-    private function run()
-    {
-        if(empty($this->routineList)){
-            $this->stopTick();
-            return;
-        }
+    private function run(){
+        if(empty($this->routineList)){
+            $this->stopTick();
+            return;
+        }
 
-        foreach ($this->routineList as $k => $task) {
-            $task->run();
+        foreach ($this->routineList as $k => $task) {
+            $task->run();
 
-            if($task->isFinished()){
-                unset($this->routineList[$k]);
-            }
-        }
-    }
-    
+            if($task->isFinished()){
+                unset($this->routineList[$k]);
+            }
+        }
+    }
 }
 
-class Task
-{
-    protected $stack;
-    protected $routine;
+class Task{
+    protected $stack;
+    protected $routine;
 
-    public function __construct(Generator $routine)
-    {
-        $this->routine = $routine;
-        $this->stack = new SplStack();
-    }
+    public function __construct(Generator $routine){
+        $this->routine = $routine;
+        $this->stack = new SplStack();
+    }
 
-    /**
-     * [run 协程调度]
-     * @return [type]         [description]
-     */
-    public function run()
-    {
-        $routine = &$this->routine;
+    /**
+     * [run 协程调度]
+     * @return [type]         [description]
+     */
+    public function run(){
+        $routine = &$this->routine;
 
-        try {
+        try {
+            if(!$routine){
+                return;
+            }
 
-            if(!$routine){
-                return;
-            }
+            $value = $routine->current(); 
 
-            $value = $routine->current(); 
+            //嵌套的协程
+            if ($value instanceof Generator) {
+                $this->stack->push($routine);
+                $routine = $value;
+                return;
+            }
 
-            //嵌套的协程
-            if ($value instanceof Generator) {
-                $this->stack->push($routine);
-                $routine = $value;
-                return;
-            }
+            //嵌套的协程返回
+            if(!$routine->valid() && !$this->stack->isEmpty()) {
+                $routine = $this->stack->pop();
+            }
 
-            //嵌套的协程返回
-            if(!$routine->valid() && !$this->stack->isEmpty()) {
-                $routine = $this->stack->pop();
-            }
+            $routine->next();
 
-            $routine->next();
+        } catch (Exception $e) {
 
-        } catch (Exception $e) {
+            if ($this->stack->isEmpty()) {
+                //throw the exception 
+                return;
+            }
+        }
+    }
 
-            if ($this->stack->isEmpty()) {
-                /*
-                    throw the exception 
-                */
-                return;
-            }
-        }
-    }
-
-    /**
-     * [isFinished 判断该task是否完成]
-     * @return boolean [description]
-     */
-    public function isFinished(){
+    /**
+     * [isFinished 判断该task是否完成]
+     * @return boolean [description]
+     */
+    public function isFinished(){
         return $this->stack->isEmpty() && !$this->routine->valid();
-    }
+    }
 
     public function getRoutine(){
         return $this->routine;
